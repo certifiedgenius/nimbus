28d7a39af87559ee303d19eedc0e064f1c06956f
certifiedgenius <mugeeramansoor@live.com>
2022-05-07 17:49:07 +0200

    Content section outline

--- wp-content/themes/mugeerastartingtemplate/front-page.php
+++ wp-content/themes/mugeerastartingtemplate/front-page.php
@@ -1,7 +1,13 @@
 <?php get_header() ;?>
 
 	<!-- search bar under the nav -->
-	<div class="flex flex-row-reverse items-center px-8"> <?php get_search_form(); ?> </div>
+	<section class="md:container md:mx-auto">
+
+		<div class="container">
+
+			<div class="flex flex-row-reverse items-center px-8"> <?php get_search_form(); ?> </div>
+		</div>
+	</section>
 
 
 
@@ -49,9 +55,56 @@
 
 
 	<!-- 3 Buttons for the diff categories after hero -->
-	<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Get started</a>
-	<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Get started</a>
-	<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Get started</a>
+	<section class="md:container md:mx-auto">
+		<div class="container flex justify-evenly py-4 px-8">
+			<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Träningsläger</a>
+			<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Cuper</a>
+			<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Fotbollsresor</a>
+
+		</div>
+	</section>
+
+
+
+
+<!-- Featured content section -->
+<section class="md:container md:mx-auto">
+    <div class="container relative items-center w-full px-5 py-12 mx-auto md:px-12 lg:px-24 max-w-7xl">
+
+        <div class="grid w-full grid-cols-1 gap-6 mx-auto lg:grid-cols-3">
+            <div class="p-6">
+                <img class="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="/wp-content/themes/mugeerastartingtemplate/assets/images/squareCard.png" alt="blog">
+
+                <h1 class="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">Short headline.</h1>
+                <p class="mx-auto text-base leading-relaxed text-gray-500">Free and Premium themes, UI Kit's, templates and landing pages built with Tailwind CSS, HTML &amp; Next.js.</p>
+
+                <div class="mt-4">
+                    <a href="#" class="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-neutral-600" title="read more"> Read More » </a>
+                </div>
+            </div>
+            <div class="p-6">
+                <img class="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="/wp-content/themes/mugeerastartingtemplate/assets/images/squareCard.png" alt="blog">
+
+                <h1 class="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">Short headline.</h1>
+                <p class="mx-auto text-base leading-relaxed text-gray-500">Free and Premium themes, UI Kit's, templates and landing pages built with Tailwind CSS, HTML &amp; Next.js.</p>
+
+                <div class="mt-4">
+                    <a href="#" class="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-neutral-600" title="read more"> Read More » </a>
+                </div>
+            </div>
+            <div class="p-6">
+                <img class="object-cover object-center w-full mb-8 lg:h-48 md:h-36 rounded-xl" src="/wp-content/themes/mugeerastartingtemplate/assets/images/squareCard.png" alt="blog">
+
+                <h1 class="mx-auto mb-8 text-2xl font-semibold leading-none tracking-tighter text-neutral-600 lg:text-3xl">Short headline.</h1>
+                <p class="mx-auto text-base leading-relaxed text-gray-500">Free and Premium themes, UI Kit's, templates and landing pages built with Tailwind CSS, HTML &amp; Next.js.</p>
+
+                <div class="mt-4">
+                    <a href="#" class="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-neutral-600" title="read more"> Read More » </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
 
 
 
@@ -59,7 +112,5 @@
 
 
 
-
-
-	</div>
+	</div> <!--Maybe this does not have a family -->
 <?php get_footer(); ?>
--- wp-content/themes/mugeerastartingtemplate/header.php
+++ wp-content/themes/mugeerastartingtemplate/header.php
@@ -30,22 +30,29 @@
 
 
         <!--  nav bar  -->
-        <header class="flex justify-between text-black py-4 px-8">
+        <section class="md:container md:mx-auto">
 
-            <a href="#" class="text-lg font-bold">nimbus</a>
-
-                <?php wp_nav_menu([
-                    'theme_location' => 'primary',
-                    'container' => 'nav',
-                    'depth' => 1,
-                    'menu_class' => 'flex flex-row items-center align-middle gap-4',
-                ]); ?>
+            <div class="container">
 
 
-            <button href="#" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">CTA
-                <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
-                    <path d="M5 12h14M12 5l7 7-7 7"></path>
-                </svg>
-            </button>
+                <header class="flex justify-between text-black py-4 px-8">
 
-        </header>
+                    <a href="#" class="text-lg font-bold">nimbus</a>
+
+                        <?php wp_nav_menu([
+                            'theme_location' => 'primary',
+                            'container' => 'nav',
+                            'depth' => 1,
+                            'menu_class' => 'flex flex-row items-center align-middle gap-4',
+                        ]); ?>
+
+
+                    <button href="#" class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">CTA
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-4 h-4 ml-1" viewBox="0 0 24 24">
+                            <path d="M5 12h14M12 5l7 7-7 7"></path>
+                        </svg>
+                    </button>
+
+                </header>
+            </div>
+        </section>
