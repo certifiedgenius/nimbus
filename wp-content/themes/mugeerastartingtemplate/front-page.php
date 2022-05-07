3809837044bb57cdee2ab374a907534aec861a2a
certifiedgenius <mugeeramansoor@live.com>
2022-05-07 17:00:13 +0200

    Hero landig page button style change

--- wp-content/themes/mugeerastartingtemplate/front-page.php
+++ wp-content/themes/mugeerastartingtemplate/front-page.php
@@ -6,61 +6,60 @@
 
 
 
-<section class="md:container md:mx-auto">
+	<!-- The HERO at the top -->
+	<section class="md:container md:mx-auto">
 
-	<div class="container">
+		<div class="container">
 
+			<section class="relative py-20 2xl:py-11 overflow-hidden"> <!--try to add in w-full relative in the class-->
 
-
-<!-- The HERO at the top -->
-	<section class="relative py-20 2xl:py-11 overflow-hidden"> <!--try to add in w-full relative in the class-->
-
-  			<div class="container px-3 mx-auto">
+				<div class="container px-3 mx-auto">
 
 
-			  <div class="w-full h-full bg-no-repeat bg-cover bg-left bg-fixed">
-				  <img class="" src="/wp-content/themes/mugeerastartingtemplate/assets/images/pierre.jpg" alt="Pierre blaché">
-			  </div>
-
-
-					<div class="max-w-3xl mx-auto text-center">
-
-						<div class="absolute top-11 right-3 bottom-11 left-3 overflow-hidden bg-fixed"
-						style="background-color: rgba(0, 0, 0, 0.45)">
-
-							<div class="flex justify-center items-center h-full">
-
-								<div class="text-center text-white px-6 md:px-12">
-
-									<h1 class="py-12 text-5xl font-bold mt-0 mb-96">Travel Heading</h1>
-
-										<h6 class="text-3xl font-bold mb-8">Get the best tickets with the best memories that will live with you forever.</h3>
-
-							<button
-								type="button"
-								href="#"
-								class="inline-block px-6 py-2.5 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out"
-								data-mdb-ripple="true" data-mdb-ripple-color="light">
-								Get started
-							</button>
-
-								</div>
-							</div>
-						</div>
+					<div class="w-full h-full bg-no-repeat bg-cover bg-left bg-fixed">
+						<img class="" src="/wp-content/themes/mugeerastartingtemplate/assets/images/pierre.jpg" alt="Pierre blaché">
 					</div>
 
 
+							<div class="max-w-3xl mx-auto text-center">
+
+								<div class="absolute top-11 right-3 bottom-11 left-3 overflow-hidden bg-fixed"
+								style="background-color: rgba(0, 0, 0, 0.45)">
+
+									<div class="flex justify-center items-center h-full">
+
+										<div class="text-center text-white px-6 md:px-12">
+
+											<h1 class="py-12 text-5xl font-bold mt-0 mb-96">Travel Heading</h1>
+
+												<h6 class="text-3xl font-bold mb-8">Get the best tickets with the best memories that will live with you forever.</h3>
+
+													<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Get started</a>
+													<a class="inline-block py-5 px-12 text-white font-bold border border-gray-300 hover:border-gray-200 rounded-full" href="#">Join us now</a>
+
+										</div>
+									</div>
+								</div>
+							</div>
+					</div>
+				</section>
 			</div>
+		</section>
 
 
 
-  			</div>
-</section>
+	<!-- 3 Buttons for the diff categories after hero -->
+	<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Get started</a>
+	<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Get started</a>
+	<a class="inline-block mb-4 sm:mb-0 sm:mr-4 py-5 px-12 text-white font-bold bg-blue-500 hover:bg-blue-600 rounded-full transition duration-200" href="#">Get started</a>
+
+
+
+
 
 
 
 
 
 	</div>
-</section>
 <?php get_footer(); ?>
