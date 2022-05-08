818e989def96cd7be5bdaab61016ac76614880e0
certifiedgenius <mugeeramansoor@live.com>
2022-05-08 17:21:00 +0200

    Hero hero done and little baby hero under the big hero done too

--- wp-content/themes/mugeerastartingtemplate/templates/template-travelcamp.php
+++ wp-content/themes/mugeerastartingtemplate/templates/template-travelcamp.php
@@ -10,96 +10,108 @@ Template Name: Träningsresor
 <?php get_header();?>
 
 
+
+
+
+
     <!-- The Hero below the nav -->
-    <div id="carouselExampleCaptionsFull" class="carousel slide carousel-fade relative h-screen" data-bs-ride="carousel">
-    <div class="carousel-indicators absolute right-0 bottom-0 left-0 flex justify-center p-0 mb-4">
-        <button
-        type="button"
-        data-bs-target="#carouselExampleCaptionsFull"
-        data-bs-slide-to="0"
-        class="active"
-        aria-current="true"
-        aria-label="Slide 1"
-        ></button>
-        <button
-        type="button"
-        data-bs-target="#carouselExampleCaptionsFull"
-        data-bs-slide-to="1"
-        aria-label="Slide 2"
-        ></button>
-        <button
-        type="button"
-        data-bs-target="#carouselExampleCaptionsFull"
-        data-bs-slide-to="2"
-        aria-label="Slide 3"
-        ></button>
-    </div>
-    <div class="carousel-inner relative w-full overflow-hidden h-screen">
-        <div class="carousel-item active relative float-left w-full h-screen bg-no-repeat bg-cover bg-center">
-        <video class="min-w-full min-h-full max-w-fit xl:min-w-0 xl:min-h-0" playsinline autoplay muted loop>
-            <source class="" src="https://mdbootstrap.com/img/video/Lines.mp4" type="video/mp4" />
-        </video>
-        <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed" style="background-color: rgba(0, 0, 0, 0.6);">
-            <div class="flex justify-center items-center h-full">
-            <div class="text-white text-center px-14 px-md-0">
-                <h2 class="text-3xl font-semibold mb-4">Learn Tailwind Elements</h2>
-                <h5 class="text-lg font-semibold mb-6">Best & free guide of responsive web design</h5>
-                <div class="md:space-x-2">
-                <a type="button" class="inline-block px-6 py-2 mb-2 md:mb-0 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" href="#!" role="button"     data-mdb-ripple="true" data-mdb-ripple-color="light">Start tutorial</a>
-                <a type="button" class="inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" href="#!" role="button"     data-mdb-ripple="true" data-mdb-ripple-color="light">Read more</a>
+        <div
+            class="
+            w-full
+            h-screen
+            bg-center
+            bg-no-repeat
+            bg-cover"
+            style="background-image: url('https://images.unsplash.com/photo-1609343007774-dad98a8f8c33?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1225&q=80')">
+
+
+                <div class="w-full h-screen bg-opacity-50 bg-black flex justify-center items-center relative">
+
+                    <div class="mx-4 text-center text-white">
+
+                        <h1 class="font-bold text-6xl mb-4">Träningsresor my love</h1>
+                        <p class="font-bold text-3xl mb-12">This is a test bound 2 falling in love</p>
+                    </div>
+
+
+                    <div class="absolute bottom-14">
+                        <a
+                            href="#"
+                            class="
+                            border-2
+                            border-white
+                            rounded-md
+                            font-bold
+                            text-white
+                            px-4
+                            py-3
+                            transition
+                            duration-300
+                            ease-in-out
+                            hover:bg-blue-500
+                            hover:text-white mr-6">
+                            Kontakta oss
+                        </a>
+
+                    </div>
                 </div>
-            </div>
-            </div>
         </div>
+
+
+
+
+
+
+    <!-- Main content of the page goes under here-->
+    <section class="md:container md:mx-auto body-font">
+		<div class="container relative items-center w-full py-6 mx-auto md:px-12 lg:px-24 max-w-full">
+
+
+            <!-- hero card under the big hero hero -->
+            <section>
+                <div class="2xl:max-w-7xl sm:px-6 md:px-12 lg:px-24 lg:py-24 2xl:px-12 px-4 py-12 mx-auto">
+                    <div class="2xl:max-w-7xl flex flex-wrap items-center mx-auto">
+                    <div class="lg:flex-grow lg:w-1/2 lg:pr-24 md:mb-0 flex flex-col items-start mb-16 text-left">
+                        <span class="mb-8 text-xs font-bold tracking-widest text-blue-600 uppercase"> Your tagline </span>
+                        <h1 class="text-neutral-600 md:text-7xl lg:text-5xl mb-8 text-4xl font-bold leading-none tracking-tighter">Medium length display headline.</h1>
+                        <p class="mb-8 text-base leading-relaxed text-left text-gray-500">Free and Premium themes, UI Kit's, templates and landing pages built with Tailwind CSS, HTML &amp; Next.js.</p>
+                        <div class="prose-md prose">
+                        <ul>
+                            <li class="text-gray-500">Ain't no sunshine when she's gone.</li>
+                            <li class="text-gray-500">Expensive feature.</li>
+                            <li class="text-gray-500">Really good feauture.</li>
+                        </ul>
+                        </div>
+                    </div>
+                    <div class="lg:w-5/6 lg:max-w-lg rounded-xl xl:mt-0 w-full mt-12">
+                        <div>
+                        <div class="relative w-full max-w-lg">
+                            <div class="bg-violet-300 -left-4 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob absolute top-0 rounded-full"></div>
+
+                            <div class="bg-fuchsia-300 -bottom-24 right-20 w-72 h-72 mix-blend-multiply filter blur-xl opacity-70 animate-blob animation-delay-4000 absolute rounded-full"></div>
+                            <div class="relative">
+                            <img class="object-cover object-center mx-auto rounded-lg shadow-2xl" alt="hero" src="/wp-content/themes/mugeerastartingtemplate/assets/images/squareCard.png">
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
+
+
+
+            <!-- Trip Details Section -->
+            <h1 class="py-12 text-5xl font-bold mb-1 mt-12">Trip Details</h1>
+
+
+
+
+
         </div>
-        <div class="carousel-item relative float-left w-full h-screen bg-no-repeat bg-cover bg-center">
-        <video class="min-w-full min-h-full max-w-fit xl:min-w-0 xl:min-h-0" playsinline autoplay muted loop>
-            <source class="" src="https://mdbootstrap.com/img/video/forest.mp4" type="video/mp4" />
-        </video>
-        <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed" style="background-color: rgba(0, 0, 0, 0.3)">
-            <div class="flex justify-center items-center h-full">
-            <div class="text-white text-center px-14 px-md-0">
-                <h2 class="text-3xl font-semibold mb-4">You can place here any content</h2>
-            </div>
-            </div>
-        </div>
-        </div>
-        <div class="carousel-item relative float-left w-full h-screen bg-no-repeat bg-cover bg-center">
-        <video class="min-w-full min-h-full max-w-fit xl:min-w-0 xl:min-h-0" playsinline autoplay muted loop>
-            <source class="" src="https://mdbootstrap.com/img/video/Tropical.mp4" type="video/mp4" />
-        </video>
-        <div class="absolute top-0 right-0 bottom-0 left-0 w-full h-full overflow-hidden bg-fixed" style="background: linear-gradient(45deg, rgba(29, 236, 197, 0.7), rgba(91, 14, 214, 0.7) 100%);">
-            <div class="flex justify-center items-center h-full">
-            <div class="text-white text-center px-14 px-md-0">
-                <h2 class="text-3xl font-semibold mb-4">And cover it with any mask</h2>
-                <a type="button" class="inline-block px-6 py-2 border-2 border-white text-white font-medium text-xs leading-tight uppercase rounded hover:bg-black hover:bg-opacity-5 focus:outline-none focus:ring-0 transition duration-150 ease-in-out" href="#!" role="button"     data-mdb-ripple="true" data-mdb-ripple-color="light">Learn more</a>
-            </div>
-            </div>
-        </div>
-        </div>
-    </div>
-    <button
-        class="carousel-control-prev absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline left-0"
-        type="button"
-        data-bs-target="#carouselExampleCaptionsFull"
-        data-bs-slide="prev"
-    >
-        <span
-        class="carousel-control-prev-icon inline-block bg-no-repeat"
-        aria-hidden="true"
-        ></span>
-        <span class="visually-hidden">Previous</span>
-    </button>
-    <button
-        class="carousel-control-next absolute top-0 bottom-0 flex items-center justify-center p-0 text-center border-0 hover:outline-none hover:no-underline focus:outline-none focus:no-underline right-0"
-        type="button"
-        data-bs-target="#carouselExampleCaptionsFull"
-        data-bs-slide="next"
-    >
-        <span
-        class="carousel-control-next-icon inline-block bg-no-repeat"
-        aria-hidden="true"
-        ></span>
-        <span class="visually-hidden">Next</span>
-    </button>
-    </div>
+	</section>
+
+
+
+
+    <?php get_footer(); ?>
