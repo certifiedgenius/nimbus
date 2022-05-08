5a3fb3a147083a73eb49865fba243686b9e3c535
certifiedgenius <mugeeramansoor@live.com>
2022-05-08 20:02:40 +0200

    created template parts and move around different components and also done with the news section

--- wp-content/themes/mugeerastartingtemplate/front-page.php
+++ wp-content/themes/mugeerastartingtemplate/front-page.php
@@ -177,50 +177,8 @@
 
 
 
-	<!-- Landing Page Newsletter section -->
-	<section class="md:container md:mx-auto">
-		<div class="container relative items-center w-full py-12 mx-auto md:px-12 lg:px-24 max-w-full">
-
-			<h1 class="flex justify-center items-center py-2 text-5xl font-bold mb-1 mt-5">Newsletter</h1>
-				<p class="flex justify-center items-center py-3 mb-1 mt-5">Få uppdateringar kring spännande äventyr.</p>
-
-
-
-				<!-- Container holding both of the forms -->
-				<div class="container relative w-full py-12 mx-auto md:px-12 lg:px-24 grid place-items-center mt-11">
-
-
-					<!-- Checkboxes -->
-					<div>
-						<form action="/action-page.php" class="flex justify-start gap-6 mb-6">
-
-							<input type="checkbox" name="Sportresor" value="Sportresor">
-							<label for="Sportresor">Sportresor</label><br>
-
-							<input type="checkbox" name="Träningsläger" value="Träningsläger">
-							<label for="Träningsläger">Träningsläger</label><br>
-
-							<input type="checkbox" name="Cuper" value="Cuper">
-							<label for="Cuper">Cuper</label><br><br>
-						</form>
-					</div>
-
-
-
-					<!-- E-mail input -->
-					<div>
-						<form action="/action-page.php" class="m-4 flex">
-
-							<input type="email" name="email" class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Your E-mail"/>
-							<button type="submit" value="Subscribe" class="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r hover:bg-purple-500 transition duration-200"">Subscribe</button>
-
-						</form>
-					</div>
-
-
-				</div>
-			</div>
-	</section>
+	<!-- Newsletter Section -->
+	<?php get_template_part( slug: 'template-parts/section', name: 'newsletter' ); ?>
 
 
 
@@ -231,7 +189,7 @@
 
 	<!-- Landing Page Featured Solutions section at the bottom-->
 	<section class="md:container md:mx-auto body-font">
-		<div class="container relative items-center w-full py-6 mx-auto md:px-12 lg:px-24 max-w-full">
+		<div class="container relative items-center mb-48 w-full py-6 mx-auto md:px-12 lg:px-24 max-w-full">
 
 
 				<div class="container px-5 py-24 mx-auto">
---
+++ wp-content/themes/mugeerastartingtemplate/template-parts/section-newsletter.php
@@ -0,0 +1,44 @@
+<!-- Landing Page Newsletter section -->
+<section class="md:container md:mx-auto">
+		<div class="container relative items-center w-full py-12 mx-auto mb-40 md:px-12 lg:px-24 max-w-full">
+
+			<h1 class="flex justify-center items-center py-2 text-5xl font-bold mb-1 mt-5">Newsletter</h1>
+				<p class="flex justify-center items-center py-3 mb-1 mt-5">Få uppdateringar kring spännande äventyr.</p>
+
+
+
+				<!-- Container holding both of the forms -->
+				<div class="container relative w-full py-12 mx-auto md:px-12 lg:px-24 grid place-items-center mt-11">
+
+
+					<!-- Checkboxes -->
+					<div>
+						<form action="/action-page.php" class="flex justify-start gap-6 mb-6">
+
+							<input type="checkbox" name="Sportresor" value="Sportresor">
+							<label for="Sportresor">Sportresor</label><br>
+
+							<input type="checkbox" name="Träningsläger" value="Träningsläger">
+							<label for="Träningsläger">Träningsläger</label><br>
+
+							<input type="checkbox" name="Cuper" value="Cuper">
+							<label for="Cuper">Cuper</label><br><br>
+						</form>
+					</div>
+
+
+
+					<!-- E-mail input -->
+					<div>
+						<form action="/action-page.php" class="m-4 flex">
+
+							<input type="email" name="email" class="rounded-l-lg p-4 border-t mr-0 border-b border-l text-gray-800 border-gray-200 bg-white" placeholder="Your E-mail"/>
+							<button type="submit" value="Subscribe" class="px-8 rounded-r-lg bg-yellow-400  text-gray-800 font-bold p-4 uppercase border-yellow-500 border-t border-b border-r hover:bg-purple-500 transition duration-200"">Subscribe</button>
+
+						</form>
+					</div>
+
+
+				</div>
+			</div>
+	</section>
