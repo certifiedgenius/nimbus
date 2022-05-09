bc063d60919ce5eca31ed5a5c1bfda3a2b4e7aee
certifiedgenius <mugeeramansoor@live.com>
2022-05-09 19:06:14 +0200

    added a read more button

--- wp-content/themes/mugeerastartingtemplate/archive.php
+++ wp-content/themes/mugeerastartingtemplate/archive.php
@@ -1,31 +1,33 @@
-<?php if( have_posts() ): while( have_posts() ): the_post();?>
+<?php get_header(); ?>
 
 
-	<div class="card mb-3">
+<section class="md:container md:mx-auto body-font">
+	<div class="container relative items-center w-full py-6 mx-auto md:px-12 lg:px-24 max-w-full">
 
-		<div class="card-body d-flex justify-content-center align-items-center">
+            <?php if( have_posts() ): while( have_posts() ): the_post();?>
 
+                    <?php if(has_post_thumbnail()):?>
 
-			<?php if(has_post_thumbnail()):?>
+                        <img src="<?php the_post_thumbnail_url('blog-small');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail mr-4">
 
-				<img src="<?php the_post_thumbnail_url('blog-small');?>" alt="<?php the_title();?>" class="img-fluid mb-3 img-thumbnail mr-4">
-
-			<?php endif;?>
-
-
-			<div class="blog-content">
-
-				<h3><?php the_title();?></h3>
-				<?php the_excerpt();?>
-
-				<a href="<?php the_permalink();?>" class="btn btn-success">Läs mer</a>
-
-			</div>
+                    <?php endif;?>
 
 
-		</div>
+                        <div class="blog-content">
 
-	</div>
+                            <h3><?php the_title();?></h3>
+                            <?php the_excerpt();?>
+
+                            <div class="mt-4">
+                                <a href="<?php the_permalink();?>" class="inline-flex items-center mt-4 font-semibold text-blue-600 lg:mb-0 hover:text-neutral-600" title="Läs mer"> Läs mer » </a>
+                            </div>
+                        </div>
 
 
-<?php endwhile; else: endif;?>
+
+
+
+                <?php endwhile; else: endif;?>
+
+    </div>
+</section>
